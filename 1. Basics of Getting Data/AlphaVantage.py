8af843c631ac03e8fe4c5fc4b3eb71c48a41b9b5
monikadelekta <monikadelekta@hotmail.com>
2020-01-26 15:47:41 +0000

    Alpha vantage API use - used for intraday data every 1 minute

--- 1. Basics of Getting Data/AlphaVantage.py
+++ 1. Basics of Getting Data/AlphaVantage.py
@@ -4,6 +4,8 @@ from alpha_vantage.timeseries import TimeSeries
 # API key from alpha vantage website
 # https://www.alphavantage.co/documentation/
 
+
+# supported intervals include 1min, 5min, 15min, 30min, 60min
 ts = TimeSeries(key=open('../key.rtf','r').read(), output_format= 'pandas')
 data, meta_data = ts.get_intraday(symbol = 'AAPL', interval = '1min', outputsize = 'full')
 
