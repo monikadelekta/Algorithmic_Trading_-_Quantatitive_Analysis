afa8ea7eff648f0676bd1b2bc2f99195a9557826
monikadelekta <monikadelekta@hotmail.com>
2020-01-26 15:44:18 +0000

    Alpha vantage API use

--- 1. Basics of Getting Data/AlphaVantage.py
+++ 1. Basics of Getting Data/AlphaVantage.py
@@ -2,6 +2,7 @@ from alpha_vantage.timeseries import TimeSeries
 
 # used for intraday trading strategies
 # API key from alpha vantage website
+# https://www.alphavantage.co/documentation/
 
 ts = TimeSeries(key=open('../key.rtf','r').read(), output_format= 'pandas')
 data, meta_data = ts.get_intraday(symbol = 'AAPL', interval = '1min', outputsize = 'full')
